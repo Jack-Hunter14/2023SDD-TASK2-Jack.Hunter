f915f245e0c1c94e0544f008aa1049bd35ded859
Jack-Hunter14 <jack.hunter14@education.nsw.gov.au>
2023-08-03 11:47:02 +1000

    renamed objects and added entity

--- Code/script.js
+++ Code/script.js
@@ -1,29 +1,22 @@
-var xhttp = new XMLHttpRequest();
 callXml();
+var testName = "yessir";
 
 function callXml() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            console.log("test");
             myFunction(this);
         }
     };
     var newFileName = "content.xml?" + new Date().getTime();
-    xhttp.open("GET", newFileName);
-    if (xhttp.open("GET", newFileName) == null) {
-        console.log("null0");
-    }
+    xhttp.open("GET", newFileName, true);
     xhttp.send();
 }
 
 function myFunction(xml) {
     var xmlDoc = xml.responseXML;
-    if (xmlDoc == null) {
-        console.log("null");
-    }
     var table = "<tr><th>Text</th></tr>";
-    var x = xmlDoc.getElementsByTagName("TEST");
-    table += "<tr><td>" + x[0].getElementsByTagName("TEXT")[0].childNodes[0].nodeValue
-    document.getElementById("lefttable").innerHTML = table;
+    var x = xmlDoc.getElementsByTagName("TEMPLATE");
+    table += "<tr><td>" + x[0].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue
+    document.getElementById("list").innerHTML = table;
 }
