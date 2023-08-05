9934b01e7ac73e7db602ca25d177059f70a746f9
Jack-Hunter14 <jack.hunter14@education.nsw.gov.au>
2023-08-05 19:06:21 +1000

    added button

--- Code/script.js
+++ Code/script.js
@@ -1,5 +1,4 @@
 callXml();
-var testName = "yessir";
 
 function callXml() {
     var xhttp = new XMLHttpRequest();
@@ -17,7 +16,7 @@ function myFunction(xml) {
     var xmlDoc = xml.responseXML;
     var x = xmlDoc.getElementsByTagName("TEMPLATE");
     xmlDoc.getElementsByTagName("TITLE")[0].childNodes[0].nodeValue = "Wow";
-    table = "<tr><th>" + x[0].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue + "<tr><td>" + x[0].getElementsByTagName("DESCRIPTION")[0].childNodes[0].nodeValue + "<tr><td> <img src=\"" + x[0].getElementsByTagName("IMAGE")[0].childNodes[0].nodeValue +
-    "\" /></td></tr>";
+    table = "<tr><th>" + "yoooo" + "<tr><td>" + x[0].getElementsByTagName("DESCRIPTION")[0].childNodes[0].nodeValue + "<tr><td> <img src=\"" + x[0].getElementsByTagName("IMAGE")[0].childNodes[0].nodeValue +
+    "\" /></td></tr>"  + "<button>" + x[0].getElementsByTagName("REMOVE")[0].childNodes[0].nodeValue;
     document.getElementById("list").innerHTML = table;
 }
