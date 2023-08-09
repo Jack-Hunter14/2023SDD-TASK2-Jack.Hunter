ea97ad95b63ebf296391ee1d3b267b7b4eb33332
Jack-Hunter14 <jack.hunter14@education.nsw.gov.au>
2023-08-10 08:56:49 +1000

    added for loop and div

--- Code/script.js
+++ Code/script.js
@@ -1,5 +1,9 @@
 callXml();
 
+var titles = [ "Title", "2", "3", "4" ];
+var descriptions = [ "Description" ];
+var images = [ "https://cdn.pixabay.com/photo/2014/06/03/19/38/road-sign-361514_1280.png" ];
+
 function callXml() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
@@ -16,8 +20,10 @@ function myFunction(xml) {
     var xmlDoc = xml.responseXML;
     var x = xmlDoc.getElementsByTagName("TEMPLATE");
     xmlDoc.getElementsByTagName("TITLE")[0].childNodes[0].nodeValue = "Wow";
-    table = "<table>" + "<tr><th>" + x[0].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue + "<tr><td>" + x[0].getElementsByTagName("DESCRIPTION")[0].childNodes[0].nodeValue + "<tr><td> <img src=\"" + x[0].getElementsByTagName("IMAGE")[0].childNodes[0].nodeValue +
-    "\" /></td></tr>" + "</table>" + "<button>" + x[0].getElementsByTagName("REMOVE")[0].childNodes[0].nodeValue + "</button>";
-    console.log(table);
+    table = "";
+    for (let i = 0; i < titles.length; i++) {
+        table += "<table>" + "<tr><th>" + x[0].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue + "<tr><td>" + x[0].getElementsByTagName("DESCRIPTION")[0].childNodes[0].nodeValue + "<tr><td> <img src=\"" + x[0].getElementsByTagName("IMAGE")[0].childNodes[0].nodeValue +
+        "\" /></td></tr>" + "</table>" + "<button>" + x[0].getElementsByTagName("REMOVE")[0].childNodes[0].nodeValue + "</button>";
+    }
     document.getElementById("list").innerHTML = table;
 }
