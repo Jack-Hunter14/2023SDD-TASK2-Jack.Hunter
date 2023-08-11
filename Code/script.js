d46117a795e47839ce34e04db0e28a6913515407
Jack-Hunter14 <jack.hunter14@education.nsw.gov.au>
2023-08-11 21:35:00 +1000

    made text wrap

--- Code/script.js
+++ Code/script.js
@@ -1,6 +1,6 @@
-var titles = [ "Title", "2", "3", "4" ];
-var descriptions = [ "Description", "2", "3", "4" ];
-var images = [ "https://cdn.pixabay.com/photo/2014/06/03/19/38/road-sign-361514_1280.png", "https://cdn.pixabay.com/photo/2014/06/03/19/38/road-sign-361514_1280.png", "https://cdn.pixabay.com/photo/2014/06/03/19/38/road-sign-361514_1280.png", "https://cdn.pixabay.com/photo/2014/06/03/19/38/road-sign-361514_1280.png" ];
+var titles = [ "Title" ];
+var descriptions = [ "Description" ];
+var images = [ "https://cdn.pixabay.com/photo/2014/06/03/19/38/road-sign-361514_1280.png" ];
 addTemplates();
 
 function addTemplates() {
@@ -15,21 +15,24 @@ function addTemplates() {
 function enable() {
     document.getElementById("div").style.display = "block";
 }
-function submit(newTitle, newDesc, newImg) {
-    console.log(newImg);
+
+function submit(newTitle, newDesc) {
     document.getElementById("div").style.display = "none";
-    titles.push(newTitle);
-    descriptions.push(newDesc);
+    titles.push(newTitle.value);
+    descriptions.push(newDesc.value);
+    newTitle = "";
+    newDesc = "";
     addTemplates();
 }
+
 function remove(id) {
     titles.splice(id, 1);
     descriptions.splice(id, 1);
     images.splice(id, 1);
     addTemplates();
 }
+
 var loadFile = function(event) {
-	console.log("hi");
     var image = document.getElementById('output');
 	image = URL.createObjectURL(event.target.files[0]);
     document.getElementById("addImgDisplay").src = image;
