6d716d3c7f534621acab5a4987abd59e99f1b94f
Jack-Hunter14 <jack.hunter14@education.nsw.gov.au>
2023-08-14 12:19:52 +1000

    added color picker and failsafe for localstorage

--- Code/script.js
+++ Code/script.js
@@ -1,25 +1,32 @@
 var titles = [ "Title" ];
+var titleColorArr = [ "#000000" ]
 var descriptions = [ "Description" ];
+var descColorArr = [ "#000000" ]
 var images = [ "https://cdn.pixabay.com/photo/2014/06/03/19/38/road-sign-361514_1280.png" ];
 var saveImage = false;
-console.log(localStorage.imgList);
-loadSavedTable();
+if (localStorage.titleList != undefined) {
+    loadSavedTable();
+} else {
+    addTemplates();
+}
 
 function addTemplates() {
     table = "";
     // Add table to website based on template
     for (let i = 0; i < titles.length; i++) {
         if (images[i] == undefined) {
-            table += "<button id =" + i + " onclick=\"remove(id)\">" + "-" + "</button>" + "<table>" + "<tr><th>" + titles[i] + "<tr><td>" + descriptions[i] + "</td></tr>" + "</table>";
+            table += "<button id =" + i + " onclick=\"remove(id)\">" + "-" + "</button>" + "<table>" + "<tr><th style=\"color:" + titleColorArr[i] + "\" >" + titles[i] + "<tr><td style=\"color:" + descColorArr[i] + "\" >" + descriptions[i] + "</td></tr>" + "</table>";
         } else {
-            table += "<button id =" + i + " onclick=\"remove(id)\">" + "-" + "</button>" + "<table>" + "<tr><th>" + titles[i] + "<tr><td>" + descriptions[i] + "<tr><td> <img src=\"" + images[i] +
+            table += "<button id =" + i + " onclick=\"remove(id)\">" + "-" + "</button>" + "<table>" + "<tr><th style=\"color:" + titleColorArr[i] + "\" >" + titles[i] + "<tr><td style=\"color:" + descColorArr[i] + "\" >" + descriptions[i] + "</td></tr>" + "<tr><td> <img src=\"" + images[i] +
             "\" /></td></tr>" + "</table>";
         }
     }
     document.getElementById("list").innerHTML = table;
     // Save arrays to storage
     localStorage.titleList = titles.join();
+    localStorage.titleColorList = titleColorArr.join();
     localStorage.descList = descriptions.join();
+    localStorage.descColorList = descColorArr.join();
     localStorage.imgList = images.join();
 }
 
@@ -29,10 +36,12 @@ function enable() {
 }
 
 // Save title, description, and when clicking "submit"
-function submit(newTitle, newDesc, newImage) {
+function submit(newTitle, newDesc, newImage, newTitleColor, newDescColor) {
     document.getElementById("div").style.display = "none";
     titles.push(newTitle);
+    titleColorArr.push(newTitleColor);
     descriptions.push(newDesc);
+    descColorArr.push(newDescColor);
     if (saveImage) {
         images.push(newImage);
     }
@@ -40,6 +49,8 @@ function submit(newTitle, newDesc, newImage) {
     document.getElementById("inputTitle").value = "";
     document.getElementById("inputDesc").value = "";
     document.getElementById("addImgDisplay").src = "https://cdn-icons-png.flaticon.com/512/4211/4211763.png";
+    document.getElementById("titleColor").value = "#000000"
+    document.getElementById("descColor").value = "#000000"
     addTemplates();
 }
 
@@ -63,9 +74,12 @@ var loadFile = function(event) {
 function loadSavedTable() {
     var ttl = localStorage.titleList;
     titles = ttl.split(",");
+    var ttlcol = localStorage.titleColorList;
+    titleColorArr = ttlcol.split(",");
     var dsc = localStorage.descList;
     descriptions = dsc.split(",");
-    console.log(localStorage.imgList);
+    var dsccol = localStorage.descColorList;
+    descColorArr = dsccol.split(",");
     images = localStorage.imgList.split(",");
     addTemplates();
 }
