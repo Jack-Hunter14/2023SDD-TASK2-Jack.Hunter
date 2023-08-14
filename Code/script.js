ff6d2dee798367b3d28f6f8e60c31702230cc226
Jack-Hunter14 <jack.hunter14@education.nsw.gov.au>
2023-08-14 11:41:15 +1000

    moved image adding around and added comments

--- Code/script.js
+++ Code/script.js
@@ -1,11 +1,13 @@
 var titles = [ "Title" ];
 var descriptions = [ "Description" ];
 var images = [ "https://cdn.pixabay.com/photo/2014/06/03/19/38/road-sign-361514_1280.png" ];
+var saveImage = false;
 console.log(localStorage.imgList);
 loadSavedTable();
 
 function addTemplates() {
     table = "";
+    // Add table to website based on template
     for (let i = 0; i < titles.length; i++) {
         if (images[i] == undefined) {
             table += "<button id =" + i + " onclick=\"remove(id)\">" + "-" + "</button>" + "<table>" + "<tr><th>" + titles[i] + "<tr><td>" + descriptions[i] + "</td></tr>" + "</table>";
@@ -15,25 +17,33 @@ function addTemplates() {
         }
     }
     document.getElementById("list").innerHTML = table;
+    // Save arrays to storage
     localStorage.titleList = titles.join();
     localStorage.descList = descriptions.join();
     localStorage.imgList = images.join();
 }
 
+    // Show "Add List Item" page
 function enable() {
     document.getElementById("div").style.display = "block";
 }
 
-function submit(newTitle, newDesc) {
+// Save title, description, and when clicking "submit"
+function submit(newTitle, newDesc, newImage) {
     document.getElementById("div").style.display = "none";
     titles.push(newTitle);
     descriptions.push(newDesc);
+    if (saveImage) {
+        images.push(newImage);
+    }
+    saveImage = false;
     document.getElementById("inputTitle").value = "";
     document.getElementById("inputDesc").value = "";
     document.getElementById("addImgDisplay").src = "https://cdn-icons-png.flaticon.com/512/4211/4211763.png";
     addTemplates();
 }
 
+// Remove template upon clicking "minus"
 function remove(id) {
     titles.splice(id, 1);
     descriptions.splice(id, 1);
@@ -41,13 +51,15 @@ function remove(id) {
     addTemplates();
 }
 
+// Change "add image" button
 var loadFile = function(event) {
     var image = document.getElementById('output');
 	image = URL.createObjectURL(event.target.files[0]);
     document.getElementById("addImgDisplay").src = image;
-    images.push(image);
+    saveImage = true;
 };
 
+// Load list upon reload from storage
 function loadSavedTable() {
     var ttl = localStorage.titleList;
     titles = ttl.split(",");
